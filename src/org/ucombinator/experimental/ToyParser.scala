d8d19e15703f74bb83757ede95072dec6fe0f4f8
Petey <petey.aldous@utah.edu>
2013-07-05 13:59:48 -0600

    parsing seems to be working

--- src/org/ucombinator/experimental/ToyParser.scala
+++ src/org/ucombinator/experimental/ToyParser.scala
@@ -4,33 +4,40 @@ import scala.util.parsing.combinator._
 
 object ToyParser extends RegexParsers {
 
-  // TODO come up with a way to track line numbers
-  def labelStatement: Parser[Statement] = "(label" ~> label <~ ")" ^^ { LabelStatement(1, _) }
-  def gotoStatement: Parser[Statement] = "(goto" ~> label <~ ")" ^^ { GotoStatement(1, _) }
-  def assignStatement: Parser[Statement] = "(:=" ~> variable ~ expr <~ ")" map {
-    case variable ~ expression => AssignmentStatement(1, variable, expression)
+  def labelStatement(ln: Int): Parser[Statement] = "(label" ~> label <~ ")" ^^ { LabelStatement(ln, _) }
+  def gotoStatement(ln: Int): Parser[Statement] = "(goto" ~> label <~ ")" ^^ { GotoStatement(ln, _) }
+  def assignStatement(ln: Int): Parser[Statement] = "(:=" ~> variable ~ expr <~ ")" map {
+    case variable ~ expression => AssignmentStatement(ln, variable, expression)
     case _ => scala.sys.error("could not match assignment statement")
   }
-  def condStatement: Parser[Statement] = "(if" ~> expr ~ label <~ ")" map {
-    case expression ~ label => IfStatement(1, expression, label)
+  def condStatement(ln: Int): Parser[Statement] = "(if" ~> expr ~ label <~ ")" map {
+    case expression ~ label => IfStatement(ln, expression, label)
     case _ => scala.sys.error("could not match if statement")
   }
 
-  def label: Parser[Label] = "[a-z]*".r ^^ { new Label(_) }
-  def variable: Parser[Variable] = "[a-z]*".r ^^ { new Variable(_) }
+  def label: Parser[Label] = "_[a-z]+".r ^^ { Label(_) }
+  def variable: Parser[Variable] = "[a-z]+".r ^^ { Variable(_) }
   def expr: Parser[Expression] = addition | multiplication | comparison | variable | value
 
+  def value: Parser[Value] = "[0-9]+".r ^^ { (integer) => Value(integer.toInt) }
   def addition: Parser[Addition] = "(+" ~> expr ~ expr <~ ")" ^^ { (result) => Addition(result._1, result._2) }
   def multiplication: Parser[Multiplication] = "(*" ~> expr ~ expr <~ ")" ^^ { (result) => Multiplication(result._1, result._2) }
   def comparison: Parser[Comparison] = "(=" ~> expr ~ expr <~ ")" ^^ { (result) => Comparison(result._1, result._2) }
-  def value: Parser[Value] = """\d+""".r ^^ { (integer) => Value(integer.toInt) }
 
-  def stmt: Parser[Statement] = labelStatement | gotoStatement | assignStatement | condStatement
+  def stmt(ln: Int): Parser[Statement] = labelStatement(ln) | gotoStatement(ln) | assignStatement(ln) | condStatement(ln)
 
-  def apply(input: String): Statement = parseAll(stmt, input) match {
+  def applyStmt(input: String, ln: Int): Statement = parseAll(stmt(ln), input) match {
+    case Success(result, _) => result
+    case failure: NoSuccess => scala.sys.error(failure.msg)
+  }
+
+  def applyExpr(input: String): Expression = parseAll(expr, input) match {
+    case Success(result, _) => result
+    case failure: NoSuccess => scala.sys.error(failure.msg)
+  }
+  
+  def applyLabel(input: String): Label = parseAll(label, input) match {
     case Success(result, _) => result
     case failure: NoSuccess => scala.sys.error(failure.msg)
   }
 }
-
-// TODO test, test, test
--- src/org/ucombinator/experimental/test/ParserTester.scala
+++ src/org/ucombinator/experimental/test/ParserTester.scala
@@ -8,23 +8,42 @@ import org.ucombinator.experimental.LabelStatement
 import org.ucombinator.experimental.Label
 import org.ucombinator.experimental.GotoStatement
 import org.ucombinator.experimental.IfStatement
-
-class TestException(msg: String) extends Exception
+import org.ucombinator.experimental.Addition
+import org.ucombinator.experimental.Multiplication
+import org.ucombinator.experimental.Comparison
+import org.ucombinator.experimental.Multiplication
+import org.ucombinator.experimental.AssignmentStatement
 
 object ParserTester extends App {
-  var count = 0
+  var passed = 0
+  var run = 0
   def test(result: Boolean, tag: String): Unit = {
+    run += 1
     if (result) {
-      count += 1
+      passed += 1
     } else {
-      throw new TestException("TEST FAILED: " + tag)
+      println("TEST FAILED: " + tag)
     }
   }
 
-  test(ToyParser.apply("(label l)") == LabelStatement(1, Label("l")), "basic label")
-  test(ToyParser.apply("(goto l)") == GotoStatement(1, Label("l")), "basic goto")
-  //  test(ToyParser.apply("(:= v 1)") == AssignmentStatement(1, Variable("v"), Value(1)), "basic assignment")
-  //  test(ToyParser.apply("(if 1 l)") == IfStatement(1, Value(1), Label("l")), "basic conditional")
+  def runTests: Unit = {
+    test(ToyParser.applyExpr("2") == Value(2), "basic value")
+    test(ToyParser.applyExpr("29") == Value(29), "two-digit value")
+    test(ToyParser.applyLabel("_l") == Label("_l"), "basic label")
+    test(ToyParser.applyExpr("v") == Variable("v"), "basic variable")
+    test(ToyParser.applyExpr("(+ 1 2)") == Addition(Value(1), Value(2)), "basic addition")
+    test(ToyParser.applyExpr("(* 1 2)") == Multiplication(Value(1), Value(2)), "basic multiplication")
+    test(ToyParser.applyExpr("(= 1 2)") == Comparison(Value(1), Value(2)), "basic comparison")
+    test(ToyParser.applyExpr("(= (+ 2 3) (* 8 7))") == Comparison(Addition(Value(2), Value(3)), Multiplication(Value(8), Value(7))), "nested arithmetic")
+    test(ToyParser.applyExpr("(+ 1 v)") == Addition(Value(1), Variable("v")), "variable in addition")
+    test(ToyParser.applyStmt("(label _l)", 34) == LabelStatement(34, Label("_l")), "basic label")
+    test(ToyParser.applyStmt("(goto _l)", 4) == GotoStatement(4, Label("_l")), "basic goto")
+    test(ToyParser.applyStmt("(:= v 1)", 2) == AssignmentStatement(2, Variable("v"), Value(1)), "basic assignment")
+    test(ToyParser.applyStmt("(if 1 _l)", 169) == IfStatement(169, Value(1), Label("_l")), "basic conditional")
+    test(ToyParser.applyStmt("(:= y (+ 1 2))", 3) == AssignmentStatement(3, Variable("y"), Addition(Value(1), Value(2))), "nested addition in assignment")
 
-  println(count + " tests passed")
+    println(passed + " of " + run + " tests passed")
+  }
+  
+  runTests
 }
